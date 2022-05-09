3e4fe3d37096c400e9187ef9b9e93165546bcc1c
12rambau <pierrick.rambaud49@gmail.com>
2022-05-09 09:02:24 +0000

    create the parameter tile

--- component/tile/parameter_tile.py
+++ component/tile/parameter_tile.py
@@ -1,37 +1,53 @@
 from sepal_ui import sepalwidgets as sw
 
+from component.model import FARModel
+from component.message import cm
+
 from .fcc_view import *
 from .var_view import *
 from .compute_view import *
 
 
 class ParameterTile(sw.Tile):
-    def __init__(self, model, aoi_model):
+    def __init__(self, aoi_model):
 
         # gather the model
-        self.model = model
+        self.far_model = FARModel()
         self.aoi_model = aoi_model
 
         # create the stepper header
-        header = sw.StepperHeader(
-            children=[
-                sw.StepperStep(
-                    key=1, step=1, complete=False, editable=True, children=["fcc"]
-                ),
-                sw.StepperStep(
-                    key=2, step=2, complete=False, editable=True, children=["variables"]
-                ),
-                sw.StepperStep(
-                    key=3, step=3, complete=False, editable=True, children=["compute"]
-                ),
-            ]
+        fcc_step = sw.StepperStep(
+            key=1,
+            step=1,
+            complete=False,
+            editable=True,
+            children=[cm.param.fcc.stepper],
         )
+        var_step = sw.StepperStep(
+            key=2,
+            step=2,
+            complete=False,
+            editable=True,
+            children=[cm.param.var.stepper],
+        )
+        compute_step = sw.StepperStep(
+            key=3,
+            step=3,
+            complete=False,
+            editable=True,
+            children=[cm.param.compute.stepper],
+        )
+        header = sw.StepperHeader(children=[fcc_step, var_step, compute_step])
 
         content = sw.StepperItems(
             children=[
-                sw.StepperContent(key=1, step=1, children=[FCCView(model, aoi_model)]),
-                sw.StepperContent(key=2, step=2, children=[VarView(model)]),
-                sw.StepperContent(key=3, step=3, children=[ComputeView(model)]),
+                sw.StepperContent(
+                    key=1, step=1, children=[FCCView(self.far_model, self.aoi_model)]
+                ),
+                sw.StepperContent(key=2, step=2, children=[VarView(self.far_model)]),
+                sw.StepperContent(
+                    key=3, step=3, children=[ComputeView(self.far_model)]
+                ),
             ]
         )
 
